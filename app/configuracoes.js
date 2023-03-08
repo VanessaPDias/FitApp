3c1a3b857f0c6dbbeff54e9679ef163c2eca96e1
vanessa dias <vanevanessa.dias@gmail.com>
2023-03-08 11:46:00 +0000

    configuracao para a url da api mudar de acordo com a url do app

--- app/configuracoes.js
+++ app/configuracoes.js
@@ -1,1 +1,10 @@
-export const urlDaApi = `http://localhost:3000`;
+let api = "";
+
+if(document.URL.startsWith("http://localhost")){
+  api = "http://localhost:3000"
+} 
+else if (document.URL.endsWith("azurestaticapps.net")) {
+    api = "https://api-fitapp.azurewebsites.net"
+}
+
+export const urlDaApi = api;
